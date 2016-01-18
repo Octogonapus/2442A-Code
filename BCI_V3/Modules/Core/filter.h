cd2c2595518964727a141166dbe6359efbe1dc4b
Octogonapus <benasur@charter.net>
2016-01-18 13:24:53 -0500

    TUA Update

--- BCI_V3/Modules/Core/filter.h
+++ BCI_V3/Modules/Core/filter.h
@@ -16,10 +16,11 @@ typedef struct TUAFilter
 } TUAFilter;
 
 //Simple moving average filter
-float filter_SMA(SMAFilter *filter, float alpha);
+float filter_Init_SMA(SMAFilter *filter);
+float filter_SMA(SMAFilter *filter, const float readIn, const float alpha);
 
 //Ten-unit average filter
 void filter_Init_TUA(TUAFilter *filter);
-float filter_TUAFilter(TUAFilter *filter, float componentIn);
+float filter_TUAFilter(TUAFilter *filter, const float componentIn);
 
 #endif //FILTER_H_INCLUDED
